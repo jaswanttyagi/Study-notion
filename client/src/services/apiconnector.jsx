e5b29f1fb5b54376bcb32f616591319897508432
jaswant singh <js1178052@gmail.com>
2026-03-07 20:05:04 +0530

    TLE

--- client/src/services/apiconnector.jsx
+++ client/src/services/apiconnector.jsx
@@ -1,8 +1,6 @@
 import axios from "axios"
 
-export const axiosInstance = axios.create({
-    timeout: Number(import.meta.env.VITE_API_TIMEOUT_MS || 20000),
-});
+export const axiosInstance = axios.create({});
 
 export const apiConnector = (method , url , bodyData , headers , params)=>{
     return axiosInstance({
